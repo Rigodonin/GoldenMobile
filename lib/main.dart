45771874a4d7796e8499f00483fc66e45c9b94a8
Rigoberto Salazar <rigodonin123@gmail.com>
2026-06-15 08:50:47 -0700

    Actualizacion de main para que el nombre cambie

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Sistema de Producción GS',
+      title: 'GoldenMobil',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: const Color(0xFF1E2265),
