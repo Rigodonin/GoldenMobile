a296fd4eeeb2bc4e344c7dbf5b6851adf8561514
Rigodonin <Rigodonin@users.noreply.github.com>
2026-06-18 16:59:13 +0000

    deploy: 2e642136d350797e1cdd6ee28f45dada9907f792

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1645830308" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2764124530" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
